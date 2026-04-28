9c7cb43fa88cc98ac3dec55157241bb1a820edbc
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2026-04-28 19:06:06 +0300

    first commit

--- src/modules/contact/contact.schema.ts
+++ src/modules/contact/contact.schema.ts
@@ -1,6 +1,19 @@
-// import * as yup from "yup";
+import * as yup from "yup";
 
-// export const contactCreateSchema = yup.object({
-//     localName: yup.string().required("localName is required"),
-//     contactUserId: yup.string().required("contactUserId is required")
-// })
+export const contactCreateSchema = yup.object({
+    body: yup.object({
+        localName: yup
+            .string()
+            .trim()
+            .required("Имя контакта обязательно для заполнения")
+            .min(2, "Имя слишком короткое")
+            .max(50, "Имя слишком длинное"),
+            
+        contactUserId: yup
+            .string()
+            .required("ID пользователя обязателен")
+            .test("is-number", "contactUserId должен быть числом", (value) => {
+                return !isNaN(Number(value));
+            }),
+    }),
+});
--- src/modules/contact/types/contact.contracts.ts
+++ src/modules/contact/types/contact.contracts.ts
@@ -1,41 +1,55 @@
-// import type { NextFunction, Request, Response } from "express";
-// import { Contact } from "./contact.types";
+import type { NextFunction, Request, Response } from "express";
+import { 
+    Contact, 
+    ContactEntity, 
+    CreateContactPayload, 
+    ContactWithUser, 
+    FoundUserDto 
+} from "./contact.types";
 
 
-// export interface ContactsControllerContract{
-//     getAll: (
-// 		req: Request<object, Contact[]>,
-// 		res: Response<Contact[]>,
-// 		next: NextFunction,
-// 	) => void;
-//     getContactById: (
-//         req: Request<object, Contact, {id: string}>,
-//         res: Response<Contact>,
-//         next: NextFunction
+export interface ContactsControllerContract {
+    getAll: (
+        req: Request<object, Contact[]>, 
+        res: Response<Contact[]>, 
+        next: NextFunction
+    ) => Promise<void>;
 
-//     ) => void;
-// 	create: (
-//         req: Request<object, { localName: string, avatar?: null, contactUserId: string }>,
-//         res: Response<Contact>,
-//         next: NextFunction
-//      ) => void;
-     
-        
+    getContactById: (
+        req: Request<{ id: string }>, 
+        res: Response<ContactWithUser>, 
+        next: NextFunction
+    ) => Promise<void>;
 
-// }
-// export interface ContactsServiceContract{
-// 	getAll: (userId: number) => Contact[];
+    create: (
+        req: Request<object, any, { localName: string, contactUserId: string }>, 
+        res: Response<ContactEntity>, 
+        next: NextFunction
+    ) => Promise<void>;
 
-// 	getContactById:(
-// 		id: number,
-// 		ownerId: number
-// 	)=> Promise<Contact | null>;
-//     create: (localName: string) => Promise<Contact>
-	
-// }
-// export interface ContactsRepositoryContract{
-//     findAllByOwner(ownerId: number): Promise<Contact[]>
-//     findById: (id: number, owner: number) => Promise<Contact | null>;
-//     findUserByUsername(username:string): Promise<Contact | null>
-//     create: (data: CreateContactPayload) => Promise<Contact>
-// }
+    searchUser: (
+        req: Request<{ username: string }>, 
+        res: Response<FoundUserDto>, 
+        next: NextFunction
+    ) => Promise<void>;
+}
+
+export interface ContactsServiceContract {
+    getAll: (userId: number) => Promise<ContactEntity[]>;
+    
+    getContactById: (id: number, ownerId: number) => Promise<ContactWithUser>;
+    
+    create: (data: CreateContactPayload) => Promise<ContactEntity>;
+    
+    findUserByUsername: (username: string) => Promise<FoundUserDto>;
+}
+
+export interface ContactsRepositoryContract {
+    findAllByOwner: (ownerId: number) => Promise<ContactEntity[]>;
+    
+    findById: (id: number) => Promise<ContactWithUser | null>;
+    
+    findUserByUsername: (username: string) => Promise<FoundUserDto | null>;
+    
+    create: (data: CreateContactPayload) => Promise<ContactEntity>;
+}
